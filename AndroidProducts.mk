f7be861332edbe2996d3dad3fd0d8678c551c410
zeeshan <itexpert120@gmail.com>
2021-01-27 20:14:32 +0500

    Lunch Choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 LOCAL_PATH := device/samsung/gt58wifi
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/lineage_gt58wifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_gt58wifi-eng \
+    lineage_gt58wifi-userdebug \
+    lineage_gt58wifi-user
